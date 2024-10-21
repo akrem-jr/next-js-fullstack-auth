1b3c4280c6d98699aa4b2081dca65f09956577d4
akrem0-jr <abdulbasetadem@gmail.com>
2024-10-21 10:35:38 +0300

    first update

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+export default async function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      
+          
+        
+            Welcome, user!
+          
+          <p className="text-muted-foreground">
+           learning something 
+          </p>
+    
+      
+    </main>
+  );
+}

539c49076bfcb832e73df6202cccbde7fba8cd0c
akrem0-jr <abdulbasetadem@gmail.com>
2024-10-21 14:59:20 +0300

    updates on auth

--- auth.ts
+++ auth.ts
@@ -1,5 +1,37 @@
 import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import Github from "next-auth/providers/github";
 
-export const { handlers , signIn , signOut , auth}=NextAuth({
-    providers:[]
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+    providers: [
+        Github,
+        Credentials({
+            credentials: {
+                email: { label: "Email", type: "email", placeholder: "Email" },
+                password: { label: "Password", type: "password", placeholder: "Password" },
+            },
+            async authorize(credentials) {
+                let user = null;
+
+                // validate credentials
+              
+                // get user
+
+                user = {
+                    id: '1',
+                    name: 'Akrem jr',
+                    email: 'abbuuadam18.com',
+                    role: "admin"
+                }
+
+                if (!user) {
+                    console.log("Invalid credentials");
+                    return null;
+                }
+
+                return user;
+            }
+        })
+    ],
 })
